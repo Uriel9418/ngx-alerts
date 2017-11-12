97e1c8257193fa4eea66a0a59f0f3353cd8db654
Brannon N. Darby II <bdarby86@gmail.com>
2017-11-11 20:34:24 -0500

    fix(bin): fix prepublish script

--- bin/prepublish.js
+++ bin/prepublish.js
@@ -5,9 +5,11 @@ cpx.copy('LICENSE', 'dist/libs/ngx-alerts')
 cpx.copy('README.md', 'dist/libs/ngx-alerts')
 cpx.copy('CHANGELOG.md', 'dist/libs/ngx-alerts')
 
-const packageJson = JSON.parse(fs.readFileSync('dist/libs/ngx-alerts/package.json'))
+const packageRefs = JSON.parse(fs.readFileSync('dist/libs/ngx-alerts/package.json'))
+const packageJson = JSON.parse(fs.readFileSync('package.json'))
 delete packageJson['devDependencies']
 delete packageJson['scripts']
 delete packageJson['private']
 delete packageJson['lint-staged']
+Object.keys(packageRefs).forEach(ref => (packageJson[ref] = packageRefs[ref]))
 fs.writeFileSync('dist/libs/ngx-alerts/package.json', JSON.stringify(packageJson, null, 2))

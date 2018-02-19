aa71042d3c3941b99c05b8706d522497b1e5115e
Brannon N. Darby II <bdarby86@gmail.com>
2018-02-19 14:59:44 -0500

    fix: styles

---
+++ bin/prepublish.js
@@ -0,0 +1,16 @@
+const cpx = require('cpx')
+const fs = require('fs')
+
+cpx.copy('LICENSE', 'dist/libs/ngx-alerts')
+cpx.copy('README.md', 'dist/libs/ngx-alerts')
+cpx.copy('CHANGELOG.md', 'dist/libs/ngx-alerts')
+
+const packageJson = JSON.parse(fs.readFileSync('package.json'))
+delete packageJson['devDependencies']
+delete packageJson['scripts']
+delete packageJson['private']
+delete packageJson['lint-staged']
+fs.writeFileSync(
+  'dist/libs/ngx-alerts/package.json',
+  JSON.stringify(packageJson, undefined, 2),
+)
